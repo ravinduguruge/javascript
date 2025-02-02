1bfcf703cb73902ceac1386d76bfd527f52e2ba0
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-02 21:03:05 +0530

    method

--- 05-Object Literals/objectLiterals/script.js
+++ 05-Object Literals/objectLiterals/script.js
@@ -1,13 +1,32 @@
+// this is the object create *************************************
 let user = {
     name: "Ravindu",
     email:"ravinduguruge@gmail.com",
     Dob: "2001/11/14",
     address: "Kegalle",
-    isLoggedIn: false
+    age:25,
+    isLoggedIn: false,
+    // this is the method ****************************************
+    // login: function(){
+    //     console.log(`login`);
+    // },
+    login () {
+        // console.log(`login ${name}`);
+        return `login`;
+    },
+    logout(){
+        console.log(`Logout`)
+    }
+   
 };
+
 
 console.log(user);
 console.log(typeof user );
 
-let key = "name";
+let key = "email";
 console.log(user[key]);
+
+// user.login();
+console.log(user.login())
+user.logout();
