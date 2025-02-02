372ec22a3403ac598a25f500cbf89e0ad589ad2f
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-02 20:45:01 +0530

    object

---
+++ 05-Object Literals/objectLiterals/script.js
@@ -0,0 +1,13 @@
+let user = {
+    name: "Ravindu",
+    email:"ravinduguruge@gmail.com",
+    Dob: "2001/11/14",
+    address: "Kegalle",
+    isLoggedIn: false
+};
+
+console.log(user);
+console.log(typeof user );
+
+let key = "name";
+console.log(user[key]);
