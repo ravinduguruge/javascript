a2479fd7d7743fa014ffd994104813d2837feeec
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-06 00:36:46 +0530

    DOM

---
+++ 06-Document object model/query selector/script.js
@@ -0,0 +1,10 @@
+let discription = document.querySelector('p');
+console.log(discription);
+
+// class name : .classname 
+// Id name : #Idname
+// or 
+// inspec > selectelement > right click > copy > copy selecter > paste
+
+let descriptionNew = document.querySelectorAll('p');
+console.log(descriptionNew);
