c6ce10bbb17a112152f01c995b82f86720accd56
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-06 18:25:25 +0530

    select all foreach

--- 06-Document object model/query selector/script.js
+++ 06-Document object model/query selector/script.js
@@ -1,10 +1,16 @@
+// this is selector is only one tag selector
+// cant create foreach methood 
 let discription = document.querySelector('p');
 console.log(discription);
-
 // class name : .classname 
 // Id name : #Idname
 // or 
 // inspec > selectelement > right click > copy > copy selecter > paste
 
+
+
+// can used create foreach methood 
+// this is select multiple items 
 let descriptionNew = document.querySelectorAll('p');
 console.log(descriptionNew);
+console.log(descriptionNew.forEach(element => console.log(element)))
