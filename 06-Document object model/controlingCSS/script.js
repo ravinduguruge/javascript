f5ed06dcf0463270f4f8edc36208a5f5ba135d4d
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-07 21:29:59 +0530

    controlcss

---
+++ 06-Document object model/controlingCSS/script.js
@@ -0,0 +1,12 @@
+// select class 
+const company = document.querySelector(`.abc`);
+// print now style attribute 
+console.log(company.style);
+// set and change new style attribute
+company.style.padding = "100px";
+company.style.border = "1px solid red";
+// then print it 
+console.log(company.style.padding);
+
+
+
