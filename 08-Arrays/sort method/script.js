2263d08af2d0917a6ae8e7bc5c53d4480c96af66
ravinduguruge <ravinduguruge786@gmail.com>
2025-02-18 14:17:40 +0530

    sort

---
+++ 08-Arrays/sort method/script.js
@@ -0,0 +1,15 @@
+// let numbers = [15,23,4,57,95,18,58];
+// numbers.sort();
+// console.log(numbers);
+
+let numbers = [15,23,4,57,95,18,58,1 ];
+
+// get a, b variable to first element and next element in an array 
+// then return a-b
+// if answer will be negative number should be sort these 2 number in an array 
+
+numbers.sort((a, b) => {
+    return a-b;
+});
+
+console.log(numbers)
